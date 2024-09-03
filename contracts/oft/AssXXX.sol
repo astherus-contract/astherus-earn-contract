025a0fc8a7f5d0abe0d19e3bbf1f86ab65b14aff
mark <mark.c@apollox.com>
2024-09-03 10:51:14 +0800

    fix AssXXX contract，add ERC20Pausable

--- contracts/oft/AssXXX.sol
+++ contracts/oft/AssXXX.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.25;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {OFT} from "@layerzerolabs/oft-evm/contracts/OFT.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+
+import {ERC20Pausable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
 //contract AssXXX is OFT, ERC20Burnable, AccessControl, ERC20Pausable, Ownable {
-contract AssXXX is OFT, AccessControl, Pausable {
+contract AssXXX is OFT, AccessControl, ERC20Pausable {
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant MINTER_AND_BURN_ROLE = keccak256("MINTER_AND_BURN_ROLE");
 
@@ -38,5 +40,9 @@ contract AssXXX is OFT, AccessControl, Pausable {
     function burn(address from, uint256 amount) external onlyRole(MINTER_AND_BURN_ROLE) {
         require(amount > 0, "ERC20: burn zero amount");
         _burn(from, amount);
+    }
+
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) whenNotPaused {
+        super._update(from, to, value);
     }
 }
